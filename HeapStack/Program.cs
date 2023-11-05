93020b024ff389648d95ef202468f6c0860a75e4
TeymurDevv <transmissiyaservis@gmail.com>
2023-11-05 12:20:28 +0400

    Task2 Completed

--- HeapStack/Program.cs
+++ HeapStack/Program.cs
@@ -18,3 +18,21 @@ string GettingNumbersBetweenOddNumbers(int x, int y, int counter)
 
 Console.WriteLine(GettingNumbersBetweenOddNumbers(num1, num2, counter));
 #endregion
+#region 2ci
+//2)Verilmish arrayin icindeki tek ededlerin cemini tapin.
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 45, 67 };
+int counter2 = 0;
+
+string GettingNumbersBetweenOddNumbersFromArray(int[] numbers, int counter)
+{
+    foreach (int n in numbers)
+    {
+        if (n % 2 == 1)
+        {
+            counter += n;
+        }
+    }
+    return ($"{counter}bunlarin cemini ifade dir");
+}
+Console.WriteLine(GettingNumbersBetweenOddNumbersFromArray(numbers, counter2));
+#endregion
